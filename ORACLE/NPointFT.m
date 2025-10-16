26301462abf6571d0ba8d6fc570b6ae140d64583
Joseph Woods <jgmbwoods@gmail.com>
2025-10-16 10:19:14 +0200

    Variable anme change and function description added

--- ORACLE/NPointFT.m
+++ ORACLE/NPointFT.m
@@ -1,37 +1,46 @@
+function data_mode = NPointFT(data, mode, PCycInc, bnorm)
 % Description: 
-% DFT over RF phase increments to obtain Fourier coefficients
-
-function Gp = NPointFT(MatInput, order, phi, bnorm)
+% DFT over bSSFP RF phase cycle increments to obtain Fourier coefficients (bSSFP modes)
+%
+% in:
+%      data    - complex phase-cycled bSSFP data [length(PCycInc),1] or [1,length(PCycInc)] or [x,y,z,length(PCycInc)]
+%      mode    - mode index (e.g., -1, 0, 1)
+%      PCycInc - phase cycle increments (radians)
+%      bnorm   - normalize by 1/N (for N phase cycles) (default = true)
+%
+% out:
+%      data_mode - complex mode bSSFP data scalar or [x,y,z,1]
+%
 
 if ~exist('bnorm','var') || isempty(bnorm)
     bnorm = true;
 end
 
-if isvector(MatInput)
+if isvector(data)
 
-    MatInput = MatInput(:);
-    phi      = phi(:);
+    data    = data(:);
+    PCycInc = PCycInc(:);
 
-    if numel(MatInput) ~= numel(phi)
+    if numel(data) ~= numel(PCycInc)
         error('MatInput and phi must have the same number of elements!');
     end
 
-    N  = numel(MatInput);
-    Gp = (1/N) * sum( MatInput(:) .* exp(1i.*order.*phi(:)) );
+    N         = numel(data);
+    data_mode = (1/N) * sum( data(:) .* exp(1i.*mode.*PCycInc(:)) );
 
-elseif ndims(MatInput) == 4 % 3D image with phase cycles in 4th dimension
+elseif ndims(data) == 4 % 3D image with phase cycles in 4th dimension
 
-    N  = size(MatInput,4);
+    N  = size(data,4);
 
-    if numel(phi) ~= N
+    if numel(PCycInc) ~= N
         error('size(MatInput,4) and numel(phi) must be the same!');
     end
 
-    phi = reshape(phi,1,1,1,N);
-    Gp = sum( MatInput .* exp(1i*order*phi) , 4 );
+    PCycInc   = reshape(PCycInc,1,1,1,N);
+    data_mode = sum( data .* exp(1i*mode*PCycInc) , 4 );
 
     if bnorm
-        Gp = (1/N) * Gp;
+        data_mode = (1/N) * data_mode;
     end
 
 end
